f34055a285519265be906ca4070d13f6cff16820
Damian Zaremba <damian@damianzaremba.co.uk>
2013-02-18 01:43:12 +0000

    Collect everything - we can't load js cross origin

--- update.py
+++ update.py
@@ -3,9 +3,12 @@ import urllib2
 import json
 import logging
 import sys
+from urllib import quote
 from BeautifulSoup import BeautifulSoup
 
 ABOUT_PAGE = "https://github.com/about/team"
+USER_API = "https://api.github.com/users/%s"
+OPT_ARGS = "client_id=something&client_secret=something"
 
 formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 stdout_handler = logging.StreamHandler(sys.stdout)
@@ -14,14 +17,14 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(stdout_handler)
 
-def get_employees():
+def get_hubbers():
     '''
     Parse the about page since this isn't exposed via the api :(
     '''
     hubbers = []
 
     try:
-        req = urllib2.Request(ABOUT_PAGE)
+        req = urllib2.Request('%s?%s' % (ABOUT_PAGE, OPT_ARGS))
         raw_data = urllib2.urlopen(req).read()
     except urllib2.HTTPError:
         logging.exception("Could not get about page")
@@ -34,9 +37,54 @@ def get_employees():
 
     return hubbers
 
-if __name__ == "__main__":
-    employees = get_employees()
-    with open('hubbers.js', 'w') as fh:
-        fh.write(json.dumps(employees))
-        fh.close()
+def convert_location_to_latlng(location):
+    loc = quote(location)
+    try:
+        req = urllib2.Request("http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false" % loc)
+        raw_data = urllib2.urlopen(req).read()
+    except urllib2.HTTPError:
+        logging.exception("Could not get location for %s" % loc)
+        return False
 
+    data = json.loads(raw_data)
+    if len(data) > 0:
+        lng = data['results'][0]['geometry']['location']['lng']
+        lat = data['results'][0]['geometry']['location']['lat']
+        return (lng, lat)
+
+
+def get_user_profile(user):
+    user = quote(user)
+    try:
+        req = urllib2.Request('%s?%s' % (USER_API % user, OPT_ARGS))
+        raw_data = urllib2.urlopen(req).read()
+    except urllib2.HTTPError:
+        logging.exception("Could not get profile for %s" % user)
+    else:
+        data = json.loads(raw_data)
+
+        profile = {
+            'username': user,
+            'avatar': data['avatar_url'],
+            'name': data['name'],
+        }
+
+        if 'location' in data.keys() and data['location']:
+            (lng, lat) = convert_location_to_latlng(data['location'].encode('utf-8'))
+            profile['location_lat'] = lat
+            profile['location_lng'] = lng
+
+            return profile
+    return False
+
+if __name__ == "__main__":
+    hubbers = {}
+    
+    for hubber in get_hubbers():
+        profile = get_user_profile(hubber)
+        if profile:
+            hubbers[hubber] = profile
+
+    with open('hubbers.js', 'w') as fh:
+        fh.write(json.dumps(hubbers))
+        fh.close()
